0b128c8e5fac6524110d23eb706a10f29664ab4f
Daniel Pekarek <pekarekdaniel@gmail.com>
2019-11-25 18:15:24 +0100

    Improve watchpoint view

--- projects/org.graalvm.vm.x86.trcview/src/org/graalvm/vm/x86/trcview/ui/Watches.java
+++ projects/org.graalvm.vm.x86.trcview/src/org/graalvm/vm/x86/trcview/ui/Watches.java
@@ -18,6 +18,8 @@ import javax.swing.table.AbstractTableModel;
 
 import org.graalvm.vm.util.HexFormatter;
 import org.graalvm.vm.util.OctFormatter;
+import org.graalvm.vm.x86.trcview.analysis.memory.MemoryNotMappedException;
+import org.graalvm.vm.x86.trcview.decode.DecoderUtils;
 import org.graalvm.vm.x86.trcview.expression.EvaluationException;
 import org.graalvm.vm.x86.trcview.expression.ExpressionContext;
 import org.graalvm.vm.x86.trcview.expression.Parser;
@@ -31,8 +33,9 @@ import org.graalvm.vm.x86.trcview.net.TraceAnalyzer;
 public class Watches extends JPanel {
     private static final Font FONT = new Font(Font.MONOSPACED, Font.PLAIN, 11);
 
-    private List<String> watches;
-    private List<Expression> expressions;
+    private static final String[] COLUMN_NAMES = {"Name", "Expression", "Format", "Value"};
+
+    private List<Watch> watches;
     private Model model;
     private TraceAnalyzer trc;
     private StepEvent step;
@@ -43,7 +46,6 @@ public class Watches extends JPanel {
         super(new BorderLayout());
         this.status = status;
         watches = new ArrayList<>();
-        expressions = new ArrayList<>();
 
         JTable table = new JTable(model = new Model());
         table.setFont(FONT);
@@ -56,8 +58,7 @@ public class Watches extends JPanel {
         JButton remove = new JButton("-");
         addend.addActionListener(e -> {
             int index = table.getSelectedRow();
-            watches.add("0");
-            expressions.add(new ValueNode(0));
+            watches.add(new Watch("0", new ValueNode(0)));
             model.changed();
             remove.setEnabled(true);
             if (index != -1) {
@@ -69,8 +70,7 @@ public class Watches extends JPanel {
             if (index == -1) {
                 index = watches.size();
             }
-            watches.add(index, "0");
-            expressions.add(index, new ValueNode(0));
+            watches.add(index, new Watch("0", new ValueNode(0)));
             model.changed();
             remove.setEnabled(true);
             table.setRowSelectionInterval(index, index);
@@ -82,7 +82,6 @@ public class Watches extends JPanel {
                     index = watches.size() - 1;
                 }
                 watches.remove(index);
-                expressions.remove(index);
                 model.changed();
                 if (watches.isEmpty()) {
                     remove.setEnabled(false);
@@ -134,11 +133,29 @@ public class Watches extends JPanel {
         }
     }
 
+    private String bytes(long addr) {
+        if (addr == 0) {
+            return "NULL";
+        }
+        try {
+            StringBuilder buf = new StringBuilder();
+            long ptr = addr;
+            for (int i = 0; i < 32; i++) {
+                int b = Byte.toUnsignedInt(trc.getI8(ptr++, step.getStep()));
+                buf.append(HexFormatter.tohex(b, 2));
+                buf.append(' ');
+            }
+            return buf.toString().trim();
+        } catch (MemoryNotMappedException e) {
+            return "0x" + HexFormatter.tohex(addr);
+        }
+    }
+
     @SuppressWarnings("serial")
     private class Model extends AbstractTableModel {
         @Override
         public int getColumnCount() {
-            return 2;
+            return 4;
         }
 
         @Override
@@ -148,50 +165,136 @@ public class Watches extends JPanel {
 
         @Override
         public Object getValueAt(int row, int column) {
-            if (column == 0) {
-                return watches.get(row);
-            } else {
-                Expression expr = expressions.get(row);
-                long value = eval(expr);
-                if (format.numberfmt == StepFormat.NUMBERFMT_HEX) {
-                    return "0x" + HexFormatter.tohex(value) + " [" + value + "]";
-                } else if (format.numberfmt == StepFormat.NUMBERFMT_OCT) {
-                    return OctFormatter.tooct(value) + " [" + value + "]";
-                } else {
-                    return format.formatWord(value) + " [" + value + "]";
-                }
+            Watch watch = watches.get(row);
+            Expression expr = watch.expr;
+            switch (column) {
+                case 0:
+                    return watch.name;
+                case 1:
+                    return watch.str;
+                case 2:
+                    return watch.type;
+                case 3:
+                    long value = eval(expr);
+                    if (format == null) {
+                        return null;
+                    }
+                    if (watch.type == null) {
+                        if (format.numberfmt == StepFormat.NUMBERFMT_HEX) {
+                            return "0x" + HexFormatter.tohex(value) + " [" + value + "]";
+                        } else if (format.numberfmt == StepFormat.NUMBERFMT_OCT) {
+                            return OctFormatter.tooct(value) + " [" + value + "]";
+                        } else {
+                            return format.formatWord(value) + " [" + value + "]";
+                        }
+                    } else {
+                        StringBuilder buf = new StringBuilder();
+                        boolean dec = false;
+                        for (char c : watch.type.toCharArray()) {
+                            if (dec) {
+                                switch (c) {
+                                    case 'i':
+                                    case 'n':
+                                        buf.append(format.formatWord(value));
+                                        break;
+                                    case 'd':
+                                        buf.append(value);
+                                        break;
+                                    case 'u':
+                                        buf.append(Long.toUnsignedString(value));
+                                        break;
+                                    case 'x':
+                                        buf.append(HexFormatter.tohex(value).toLowerCase());
+                                        break;
+                                    case 'X':
+                                        buf.append(HexFormatter.tohex(value).toUpperCase());
+                                        break;
+                                    case 'o':
+                                        buf.append(OctFormatter.tooct(value));
+                                        break;
+                                    case 'c':
+                                        buf.append((char) value);
+                                        break;
+                                    case 'C':
+                                        buf.append(DecoderUtils.encode((int) value & 0xFFFF));
+                                        break;
+                                    case 's':
+                                        buf.append(DecoderUtils.cstr(value, step.getStep(), trc));
+                                        break;
+                                    case 'm':
+                                        buf.append(bytes(value));
+                                        break;
+                                    default:
+                                        buf.append(c);
+                                        break;
+                                }
+                                dec = false;
+                            } else {
+                                if (c == '%') {
+                                    dec = true;
+                                } else {
+                                    buf.append(c);
+                                }
+                            }
+                        }
+                        return buf.toString();
+                    }
+                default:
+                    return null;
             }
         }
 
         @Override
         public void setValueAt(Object value, int row, int column) {
-            watches.set(row, (String) value);
-            try {
-                Expression expr = new Parser((String) value).parse();
-                expressions.set(row, expr);
+            if (column == 0) {
+                watches.get(row).name = (String) value;
+            } else if (column == 1) {
+                watches.get(row).str = (String) value;
+                try {
+                    Expression expr = new Parser((String) value).parse();
+                    watches.get(row).expr = expr;
+                    changed();
+                } catch (ParseException e) {
+                    status.accept("Parse error: " + e.getMessage());
+                    watches.get(row).expr = null;
+                }
+            } else if (column == 2) {
+                String s = (String) value;
+                if (s != null) {
+                    s = s.trim();
+                    if (s.length() == 0) {
+                        s = null;
+                    }
+                }
+                watches.get(row).type = s;
                 changed();
-            } catch (ParseException e) {
-                status.accept("Parse error: " + e.getMessage());
-                expressions.set(row, null);
             }
         }
 
         @Override
         public String getColumnName(int column) {
-            if (column == 0) {
-                return "Expression";
-            } else {
-                return "Value";
-            }
+            return COLUMN_NAMES[column];
         }
 
         @Override
         public boolean isCellEditable(int row, int column) {
-            return column == 0;
+            return true;
         }
 
         public void changed() {
             fireTableDataChanged();
+        }
+    }
+
+    private static class Watch {
+        public String name;
+        public String str;
+        public Expression expr;
+        public String type;
+
+        Watch(String str, Expression expr) {
+            this.str = str;
+            this.expr = expr;
         }
     }
 }
