4e0945b9c71d2926896d0826a23dc1915de3b090
Daniel Pekarek <pekarekdaniel@gmail.com>
2020-02-11 16:24:50 +0100

    Fix unit tests

--- projects/org.graalvm.vm.trcview.test/src/org/graalvm/vm/x86/trcview/test/storage/ParserTest.java
+++ projects/org.graalvm.vm.trcview.test/src/org/graalvm/vm/x86/trcview/test/storage/ParserTest.java
@@ -35,6 +35,8 @@ public class ParserTest {
     private List<Step> steps;
     private StepEvent lastStep;
 
+    private static final byte[] EMPTY_STATE = {0, 0, 0, 0};
+
     @Before
     public void setup() {
         arch = new MockArchitecture(false, true);
@@ -115,9 +117,9 @@ public class ParserTest {
         stepEvent(0, 0x1000, InstructionType.OTHER, new byte[]{0x42});
         stepEvent(1, 0x1001, InstructionType.OTHER, new byte[]{0x43});
         stepEvent(2, 0x1002, InstructionType.OTHER, new byte[]{0x44});
-        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, new byte[]{});
-        step(1, -1, 0x1001, StorageBackend.TYPE_OTHER, new byte[]{0x43}, new byte[]{});
-        step(2, -1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, new byte[]{});
+        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, EMPTY_STATE);
+        step(1, -1, 0x1001, StorageBackend.TYPE_OTHER, new byte[]{0x43}, EMPTY_STATE);
+        step(2, -1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, EMPTY_STATE);
         run();
     }
 
@@ -142,25 +144,25 @@ public class ParserTest {
         stepEvent(16, 0x1010, InstructionType.OTHER, new byte[]{0x52});
         stepEvent(17, 0x1011, InstructionType.CALL, new byte[]{0x53});
         stepEvent(18, 0x1012, InstructionType.OTHER, new byte[]{0x54});
-        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, new byte[]{});
-        step(1, -1, 0x1001, StorageBackend.TYPE_CALL, new byte[]{0x43}, new byte[]{});
-        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, new byte[]{});
-        step(3, 1, 0x1003, StorageBackend.TYPE_RET, new byte[]{0x45}, new byte[]{});
-        step(4, -1, 0x1004, StorageBackend.TYPE_OTHER, new byte[]{0x46}, new byte[]{});
-        step(5, -1, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, new byte[]{});
-        step(6, -1, 0x1006, StorageBackend.TYPE_CALL, new byte[]{0x48}, new byte[]{});
-        step(7, 6, 0x1007, StorageBackend.TYPE_CALL, new byte[]{0x49}, new byte[]{});
-        step(8, 7, 0x1008, StorageBackend.TYPE_OTHER, new byte[]{0x4A}, new byte[]{});
-        step(9, 7, 0x1009, StorageBackend.TYPE_CALL, new byte[]{0x4B}, new byte[]{});
-        step(10, 9, 0x100A, StorageBackend.TYPE_RET, new byte[]{0x4C}, new byte[]{});
-        step(11, 7, 0x100B, StorageBackend.TYPE_RET, new byte[]{0x4D}, new byte[]{});
-        step(12, 6, 0x100C, StorageBackend.TYPE_OTHER, new byte[]{0x4E}, new byte[]{});
-        step(13, 6, 0x100D, StorageBackend.TYPE_RET, new byte[]{0x4F}, new byte[]{});
-        step(14, -1, 0x100E, StorageBackend.TYPE_RET, new byte[]{0x50}, new byte[]{});
-        step(15, -1, 0x100F, StorageBackend.TYPE_OTHER, new byte[]{0x51}, new byte[]{});
-        step(16, -1, 0x1010, StorageBackend.TYPE_OTHER, new byte[]{0x52}, new byte[]{});
-        step(17, -1, 0x1011, StorageBackend.TYPE_CALL, new byte[]{0x53}, new byte[]{});
-        step(18, 17, 0x1012, StorageBackend.TYPE_OTHER, new byte[]{0x54}, new byte[]{});
+        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, EMPTY_STATE);
+        step(1, -1, 0x1001, StorageBackend.TYPE_CALL, new byte[]{0x43}, EMPTY_STATE);
+        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, EMPTY_STATE);
+        step(3, 1, 0x1003, StorageBackend.TYPE_RET, new byte[]{0x45}, EMPTY_STATE);
+        step(4, -1, 0x1004, StorageBackend.TYPE_OTHER, new byte[]{0x46}, EMPTY_STATE);
+        step(5, -1, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, EMPTY_STATE);
+        step(6, -1, 0x1006, StorageBackend.TYPE_CALL, new byte[]{0x48}, EMPTY_STATE);
+        step(7, 6, 0x1007, StorageBackend.TYPE_CALL, new byte[]{0x49}, EMPTY_STATE);
+        step(8, 7, 0x1008, StorageBackend.TYPE_OTHER, new byte[]{0x4A}, EMPTY_STATE);
+        step(9, 7, 0x1009, StorageBackend.TYPE_CALL, new byte[]{0x4B}, EMPTY_STATE);
+        step(10, 9, 0x100A, StorageBackend.TYPE_RET, new byte[]{0x4C}, EMPTY_STATE);
+        step(11, 7, 0x100B, StorageBackend.TYPE_RET, new byte[]{0x4D}, EMPTY_STATE);
+        step(12, 6, 0x100C, StorageBackend.TYPE_OTHER, new byte[]{0x4E}, EMPTY_STATE);
+        step(13, 6, 0x100D, StorageBackend.TYPE_RET, new byte[]{0x4F}, EMPTY_STATE);
+        step(14, -1, 0x100E, StorageBackend.TYPE_RET, new byte[]{0x50}, EMPTY_STATE);
+        step(15, -1, 0x100F, StorageBackend.TYPE_OTHER, new byte[]{0x51}, EMPTY_STATE);
+        step(16, -1, 0x1010, StorageBackend.TYPE_OTHER, new byte[]{0x52}, EMPTY_STATE);
+        step(17, -1, 0x1011, StorageBackend.TYPE_CALL, new byte[]{0x53}, EMPTY_STATE);
+        step(18, 17, 0x1012, StorageBackend.TYPE_OTHER, new byte[]{0x54}, EMPTY_STATE);
         run();
     }
 
@@ -177,16 +179,16 @@ public class ParserTest {
         stepEvent(7, 0x1007, InstructionType.OTHER, new byte[]{0x49});
         stepEvent(8, 0x1008, InstructionType.RET, new byte[]{0x4A});
         stepEvent(9, 0x1009, InstructionType.OTHER, new byte[]{0x4B});
-        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, new byte[]{});
-        step(1, -1, 0x1001, StorageBackend.TYPE_CALL, new byte[]{0x43}, new byte[]{});
-        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, new byte[]{});
-        step(3, 1, 0x1003, StorageBackend.TYPE_SYSCALL, new byte[]{0x45}, new byte[]{});
-        step(4, 3, 0x1004, StorageBackend.TYPE_OTHER, new byte[]{0x46}, new byte[]{});
-        step(5, 3, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, new byte[]{});
-        step(6, 3, 0x1006, StorageBackend.TYPE_RTI, new byte[]{0x48}, new byte[]{});
-        step(7, 1, 0x1007, StorageBackend.TYPE_OTHER, new byte[]{0x49}, new byte[]{});
-        step(8, 1, 0x1008, StorageBackend.TYPE_RET, new byte[]{0x4A}, new byte[]{});
-        step(9, -1, 0x1009, StorageBackend.TYPE_OTHER, new byte[]{0x4B}, new byte[]{});
+        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, EMPTY_STATE);
+        step(1, -1, 0x1001, StorageBackend.TYPE_CALL, new byte[]{0x43}, EMPTY_STATE);
+        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, EMPTY_STATE);
+        step(3, 1, 0x1003, StorageBackend.TYPE_SYSCALL, new byte[]{0x45}, EMPTY_STATE);
+        step(4, 3, 0x1004, StorageBackend.TYPE_OTHER, new byte[]{0x46}, EMPTY_STATE);
+        step(5, 3, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, EMPTY_STATE);
+        step(6, 3, 0x1006, StorageBackend.TYPE_RTI, new byte[]{0x48}, EMPTY_STATE);
+        step(7, 1, 0x1007, StorageBackend.TYPE_OTHER, new byte[]{0x49}, EMPTY_STATE);
+        step(8, 1, 0x1008, StorageBackend.TYPE_RET, new byte[]{0x4A}, EMPTY_STATE);
+        step(9, -1, 0x1009, StorageBackend.TYPE_OTHER, new byte[]{0x4B}, EMPTY_STATE);
         run();
     }
 
@@ -202,16 +204,16 @@ public class ParserTest {
         stepEvent(7, 0x1007, InstructionType.OTHER, new byte[]{0x49});
         stepEvent(8, 0x1008, InstructionType.RET, new byte[]{0x4A});
         stepEvent(9, 0x1009, InstructionType.OTHER, new byte[]{0x4B});
-        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, new byte[]{});
-        step(1, -1, 0x1001, StorageBackend.TYPE_CALL, new byte[]{0x43}, new byte[]{});
-        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, new byte[]{});
-        step(3, 1, 0x1003, StorageBackend.TYPE_SYSCALL, new byte[]{0x45}, new byte[]{});
-        step(4, 3, 0x1004, StorageBackend.TYPE_OTHER, new byte[]{0x46}, new byte[]{});
-        step(5, 3, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, new byte[]{});
-        step(6, 3, 0x1006, StorageBackend.TYPE_RTI, new byte[]{0x48}, new byte[]{});
-        step(7, 1, 0x1007, StorageBackend.TYPE_OTHER, new byte[]{0x49}, new byte[]{});
-        step(8, 1, 0x1008, StorageBackend.TYPE_RET, new byte[]{0x4A}, new byte[]{});
-        step(9, -1, 0x1009, StorageBackend.TYPE_OTHER, new byte[]{0x4B}, new byte[]{});
+        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, EMPTY_STATE);
+        step(1, -1, 0x1001, StorageBackend.TYPE_CALL, new byte[]{0x43}, EMPTY_STATE);
+        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, EMPTY_STATE);
+        step(3, 1, 0x1003, StorageBackend.TYPE_SYSCALL, new byte[]{0x45}, EMPTY_STATE);
+        step(4, 3, 0x1004, StorageBackend.TYPE_OTHER, new byte[]{0x46}, EMPTY_STATE);
+        step(5, 3, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, EMPTY_STATE);
+        step(6, 3, 0x1006, StorageBackend.TYPE_RTI, new byte[]{0x48}, EMPTY_STATE);
+        step(7, 1, 0x1007, StorageBackend.TYPE_OTHER, new byte[]{0x49}, EMPTY_STATE);
+        step(8, 1, 0x1008, StorageBackend.TYPE_RET, new byte[]{0x4A}, EMPTY_STATE);
+        step(9, -1, 0x1009, StorageBackend.TYPE_OTHER, new byte[]{0x4B}, EMPTY_STATE);
         run();
     }
 
@@ -225,14 +227,14 @@ public class ParserTest {
         stepEvent(4, 0x1004, InstructionType.RTI, new byte[]{0x46});
         stepEvent(5, 0x1005, InstructionType.OTHER, new byte[]{0x47});
         stepEvent(6, 0x1006, InstructionType.OTHER, new byte[]{0x48});
-        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, new byte[]{});
-        step(1, -1, 0x1001, StorageBackend.TYPE_OTHER, new byte[]{0x43}, new byte[]{});
+        step(0, -1, 0x1000, StorageBackend.TYPE_OTHER, new byte[]{0x42}, EMPTY_STATE);
+        step(1, -1, 0x1001, StorageBackend.TYPE_OTHER, new byte[]{0x43}, EMPTY_STATE);
         step(1, -1, 0x1001, StorageBackend.TYPE_TRAP, new byte[]{0x43}, new byte[]{});
-        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, new byte[]{});
-        step(3, 1, 0x1003, StorageBackend.TYPE_OTHER, new byte[]{0x45}, new byte[]{});
-        step(4, 1, 0x1004, StorageBackend.TYPE_RTI, new byte[]{0x46}, new byte[]{});
-        step(5, -1, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, new byte[]{});
-        step(6, -1, 0x1006, StorageBackend.TYPE_OTHER, new byte[]{0x48}, new byte[]{});
+        step(2, 1, 0x1002, StorageBackend.TYPE_OTHER, new byte[]{0x44}, EMPTY_STATE);
+        step(3, 1, 0x1003, StorageBackend.TYPE_OTHER, new byte[]{0x45}, EMPTY_STATE);
+        step(4, 1, 0x1004, StorageBackend.TYPE_RTI, new byte[]{0x46}, EMPTY_STATE);
+        step(5, -1, 0x1005, StorageBackend.TYPE_OTHER, new byte[]{0x47}, EMPTY_STATE);
+        step(6, -1, 0x1006, StorageBackend.TYPE_OTHER, new byte[]{0x48}, EMPTY_STATE);
         run();
     }
 }
