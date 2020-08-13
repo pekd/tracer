8c649131448cd74e807d3d85c9bbf80d9491357d
Daniel Pekarek <pekarekdaniel@gmail.com>
2020-08-13 19:14:26 +0200

    Simplify call decoder

--- projects/org.graalvm.vm.trcview.arch.x86/src/org/graalvm/vm/trcview/arch/x86/decode/AMD64CallDecoder.java
+++ projects/org.graalvm.vm.trcview.arch.x86/src/org/graalvm/vm/trcview/arch/x86/decode/AMD64CallDecoder.java
@@ -1,16 +1,25 @@
 package org.graalvm.vm.trcview.arch.x86.decode;
 
-import org.graalvm.vm.trcview.analysis.type.Function;
-import org.graalvm.vm.trcview.analysis.type.Prototype;
+import java.util.List;
+
 import org.graalvm.vm.trcview.analysis.type.Type;
 import org.graalvm.vm.trcview.arch.io.CpuState;
 import org.graalvm.vm.trcview.arch.x86.io.AMD64CpuState;
 import org.graalvm.vm.trcview.decode.CallDecoder;
-import org.graalvm.vm.trcview.expression.EvaluationException;
-import org.graalvm.vm.trcview.expression.ExpressionContext;
-import org.graalvm.vm.trcview.net.TraceAnalyzer;
 
 public class AMD64CallDecoder extends CallDecoder {
+    // TODO: implement SysV ABI
+
+    @Override
+    public long getArgument(CpuState state, int id, List<Type> types) {
+        return getRegister((AMD64CpuState) state, id);
+    }
+
+    @Override
+    public long getReturnValue(CpuState state, Type type) {
+        return ((AMD64CpuState) state).getRAX();
+    }
+
     private static long getRegister(AMD64CpuState state, int reg) {
         switch (reg) {
             case 0:
@@ -28,57 +37,5 @@ public class AMD64CallDecoder extends CallDecoder {
             default:
                 return 0;
         }
-    }
-
-    public static String decode(Function function, AMD64CpuState state, AMD64CpuState nextState, TraceAnalyzer trc) {
-        StringBuilder buf = new StringBuilder(function.getName());
-        buf.append('(');
-        Prototype prototype = function.getPrototype();
-        for (int i = 0, arg = 0; i < prototype.args.size(); i++) {
-            Type type = prototype.args.get(i);
-            long val;
-            if (type.getExpression() != null) {
-                try {
-                    ExpressionContext ctx = new ExpressionContext(state, trc);
-                    val = type.getExpression().evaluate(ctx);
-                } catch (EvaluationException e) {
-                    val = 0;
-                }
-            } else {
-                val = getRegister(state, arg++);
-            }
-            if (i > 0) {
-                buf.append(", ");
-            }
-            buf.append(str(type, val, state, trc));
-        }
-        buf.append(')');
-        if (nextState != null) {
-            long retval;
-            if (prototype.returnType.getExpression() != null) {
-                try {
-                    ExpressionContext ctx = new ExpressionContext(nextState, trc);
-                    retval = prototype.returnType.getExpression().evaluate(ctx);
-                } catch (EvaluationException e) {
-                    retval = 0;
-                }
-            } else {
-                retval = nextState.getRAX();
-            }
-            String s = str(prototype.returnType, retval, nextState, trc);
-            if (s.length() > 0) {
-                buf.append(" = ");
-                buf.append(s);
-            }
-        }
-        return buf.toString();
-    }
-
-    @Override
-    public String decode(Function function, CpuState state, CpuState nextState, TraceAnalyzer trc) {
-        if (!(state instanceof AMD64CpuState) || (nextState != null && !(nextState instanceof AMD64CpuState))) {
-            return null;
-        }
-        return decode(function, (AMD64CpuState) state, (AMD64CpuState) nextState, trc);
     }
 }
--- projects/org.graalvm.vm.trcview/src/org/graalvm/vm/trcview/decode/CallDecoder.java
+++ projects/org.graalvm.vm.trcview/src/org/graalvm/vm/trcview/decode/CallDecoder.java
@@ -3,14 +3,77 @@ package org.graalvm.vm.trcview.decode;
 import static org.graalvm.vm.trcview.decode.DecoderUtils.cstr;
 import static org.graalvm.vm.trcview.decode.DecoderUtils.ptr;
 
+import java.util.List;
+
 import org.graalvm.vm.trcview.analysis.type.Function;
+import org.graalvm.vm.trcview.analysis.type.Prototype;
 import org.graalvm.vm.trcview.analysis.type.Representation;
 import org.graalvm.vm.trcview.analysis.type.Type;
 import org.graalvm.vm.trcview.arch.io.CpuState;
+import org.graalvm.vm.trcview.expression.EvaluationException;
+import org.graalvm.vm.trcview.expression.ExpressionContext;
 import org.graalvm.vm.trcview.net.TraceAnalyzer;
 
 public abstract class CallDecoder {
-    public abstract String decode(Function function, CpuState state, CpuState nextState, TraceAnalyzer trc);
+    @SuppressWarnings("unused")
+    public long getArgument(CpuState state, int id, List<Type> types) {
+        return 0;
+    }
+
+    @SuppressWarnings("unused")
+    public long getReturnValue(CpuState state, Type type) {
+        return 0;
+    }
+
+    public String decode(Function function, CpuState state, CpuState nextState, TraceAnalyzer trc) {
+        if (state == null) {
+            return null;
+        }
+
+        StringBuilder buf = new StringBuilder(function.getName());
+        buf.append('(');
+        Prototype prototype = function.getPrototype();
+        for (int i = 0, arg = 0; i < prototype.args.size(); i++) {
+            Type type = prototype.args.get(i);
+            long val;
+            if (type.getExpression() != null) {
+                try {
+                    ExpressionContext ctx = new ExpressionContext(state, trc);
+                    val = type.getExpression().evaluate(ctx);
+                } catch (EvaluationException e) {
+                    val = 0;
+                }
+            } else {
+                val = getArgument(state, arg++, prototype.args);
+            }
+            if (i > 0) {
+                buf.append(", ");
+            }
+            buf.append(str(type, val, state, trc));
+        }
+        buf.append(')');
+
+        if (nextState != null) {
+            long retval;
+            if (prototype.returnType.getExpression() != null) {
+                try {
+                    ExpressionContext ctx = new ExpressionContext(nextState, trc);
+                    retval = prototype.returnType.getExpression().evaluate(ctx);
+                } catch (EvaluationException e) {
+                    retval = 0;
+                }
+            } else {
+                retval = getReturnValue(nextState, prototype.returnType);
+            }
+            String s = str(prototype.returnType, retval, nextState, trc);
+            if (s.length() > 0) {
+                buf.append(" = ");
+                buf.append(s);
+            }
+        }
+
+        return buf.toString();
+    }
 
     protected static String str(Type type, long val, CpuState state, TraceAnalyzer trc) {
         Representation repr = type.getRepresentation();
