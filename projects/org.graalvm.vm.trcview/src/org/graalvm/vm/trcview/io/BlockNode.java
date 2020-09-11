36915b02fe53771905c21d87beaa1ba3cbc2346c
Daniel Pekarek <pekarekdaniel@gmail.com>
2020-09-11 15:52:24 +0200

    Remove unused import

--- projects/org.graalvm.vm.trcview/src/org/graalvm/vm/trcview/io/BlockNode.java
+++ projects/org.graalvm.vm.trcview/src/org/graalvm/vm/trcview/io/BlockNode.java
@@ -45,7 +45,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import org.graalvm.vm.trcview.analysis.Analysis;
