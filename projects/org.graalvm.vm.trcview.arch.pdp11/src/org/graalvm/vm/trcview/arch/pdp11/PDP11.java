df2f63fa58ccc6dda327101988a4d8c58acb968f
Daniel Pekarek <pekarekdaniel@gmail.com>
2020-09-11 15:57:30 +0200

    Remove unused PDP11 call/syscall decoders

--- projects/org.graalvm.vm.trcview.arch.pdp11/src/org/graalvm/vm/trcview/arch/pdp11/PDP11.java
+++ projects/org.graalvm.vm.trcview.arch.pdp11/src/org/graalvm/vm/trcview/arch/pdp11/PDP11.java
@@ -1,25 +1,32 @@
 package org.graalvm.vm.trcview.arch.pdp11;
 
 import java.io.InputStream;
+import java.util.Arrays;
 
 import org.graalvm.vm.posix.elf.Elf;
 import org.graalvm.vm.trcview.arch.Architecture;
 import org.graalvm.vm.trcview.arch.io.ArchTraceReader;
 import org.graalvm.vm.trcview.arch.io.EventParser;
 import org.graalvm.vm.trcview.arch.io.StepFormat;
-import org.graalvm.vm.trcview.arch.pdp11.decode.PDP11CallDecoder;
-import org.graalvm.vm.trcview.arch.pdp11.decode.PDP11SyscallDecoder;
 import org.graalvm.vm.trcview.arch.pdp11.io.PDP11EventParser;
 import org.graalvm.vm.trcview.arch.pdp11.io.PDP11TraceReader;
+import org.graalvm.vm.trcview.decode.ABI;
 import org.graalvm.vm.trcview.decode.CallDecoder;
+import org.graalvm.vm.trcview.decode.GenericABI;
+import org.graalvm.vm.trcview.decode.GenericCallDecoder;
+import org.graalvm.vm.trcview.decode.GenericSyscallDecoder;
 import org.graalvm.vm.trcview.decode.SyscallDecoder;
+import org.graalvm.vm.trcview.expression.ast.CallNode;
+import org.graalvm.vm.trcview.expression.ast.ValueNode;
+import org.graalvm.vm.trcview.expression.ast.VariableNode;
+import org.graalvm.vm.trcview.expression.ast.XorNode;
 
 public class PDP11 extends Architecture {
     public static final short ID = Elf.EM_PDP11;
     public static final StepFormat FORMAT = new StepFormat(StepFormat.NUMBERFMT_OCT, 6, 6, 2, false);
 
-    private static final SyscallDecoder syscallDecoder = new PDP11SyscallDecoder();
-    private static final CallDecoder callDecoder = new PDP11CallDecoder();
+    private static final SyscallDecoder syscallDecoder = new GenericSyscallDecoder();
+    private static final CallDecoder callDecoder = new GenericCallDecoder();
     private static final EventParser eventParser = new PDP11EventParser();
 
     @Override
@@ -80,5 +87,18 @@ public class PDP11 extends Architecture {
     @Override
     public boolean isTaggedState() {
         return true;
+    }
+
+    @Override
+    public ABI createABI() {
+        GenericABI abi = new GenericABI();
+        abi.getCall().setArguments(Arrays.asList(new VariableNode("r0"), new VariableNode("r1"), new VariableNode("r2"), new VariableNode("r3")));
+        abi.getCall().setReturn(new VariableNode("r0"));
+        // use getU16(pc) ^ 0104000 to return the ID embedded in EMT instructions without making it
+        // indistinguishable from BPT/IOT/TRAP
+        abi.setSyscallId(new XorNode(new CallNode("getU16", Arrays.asList(new VariableNode("pc"))), new ValueNode(0104000)));
+        abi.getSyscall().setArguments(Arrays.asList(new VariableNode("r0"), new VariableNode("r1"), new VariableNode("r2"), new VariableNode("r3")));
+        abi.getSyscall().setReturn(new VariableNode("r0"));
+        return abi;
     }
 }
