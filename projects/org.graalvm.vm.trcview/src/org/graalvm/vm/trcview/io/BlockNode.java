3c21a9bf23f2cda72bc71163ae8698d9fabcf314
Daniel Pekarek <pekarekdaniel@gmail.com>
2021-11-09 20:14:21 +0100

    Remove old unused code

--- projects/org.graalvm.vm.trcview/src/org/graalvm/vm/trcview/io/BlockNode.java
+++ projects/org.graalvm.vm.trcview/src/org/graalvm/vm/trcview/io/BlockNode.java
@@ -40,26 +40,18 @@
  */
 package org.graalvm.vm.trcview.io;
 
-import java.io.EOFException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.graalvm.vm.trcview.analysis.Analysis;
-import org.graalvm.vm.trcview.arch.io.DeviceDefinitionEvent;
-import org.graalvm.vm.trcview.arch.io.Event;
-import org.graalvm.vm.trcview.arch.io.IncompleteTraceStep;
 import org.graalvm.vm.trcview.arch.io.InterruptEvent;
 import org.graalvm.vm.trcview.arch.io.StepEvent;
-import org.graalvm.vm.trcview.arch.io.TraceReader;
 import org.graalvm.vm.util.log.Levels;
 import org.graalvm.vm.util.log.Trace;
 
 public class BlockNode extends Node implements Block {
     private static final Logger log = Trace.create(BlockNode.class);
-    private static final int PROGRESS_UPDATE = 10_000;
 
     private StepEvent head;
     private List<Node> children;
@@ -166,216 +158,6 @@ public class BlockNode extends Node implements Block {
         }
         log.log(Levels.WARNING, "No step event found! " + children.size() + " children");
         return null;
-    }
-
-    public static BlockNode read(TraceReader in, Analysis analysis) throws IOException {
-        return read(in, analysis, null);
-    }
-
-    public static BlockNode read(TraceReader in, Analysis analysis, ProgressListener progress) throws IOException {
-        List<Node> nodes = new ArrayList<>();
-        Node node;
-        int tid = -1;
-        int cnt = 0;
-        while ((node = parseRecord(in, analysis, progress, tid, false)) != null) {
-            if (isStep(node)) {
-                nodes.add(node);
-            }
-            if (tid == -1) {
-                tid = node.getTid();
-            }
-            if (progress != null && cnt > PROGRESS_UPDATE) {
-                cnt = 0;
-                progress.progressUpdate(in.tell());
-            } else {
-                cnt++;
-            }
-        }
-        return new BlockNode(null, nodes);
-    }
-
-    private static boolean isStep(Node node) {
-        if (node instanceof BlockNode) {
-            return true;
-        } else if (node instanceof StepEvent) {
-            return true;
-        }
-        return false;
-    }
-
-    private static Node parseRecord(TraceReader in, Analysis analysis, ProgressListener progress, int thread, boolean ignoreTrap) throws IOException {
-        boolean system = in.getArchitecture().isSystemLevel();
-        boolean stackedTraps = in.getArchitecture().isStackedTraps();
-        int tid = thread;
-        Event event = null;
-        try {
-            event = in.read();
-        } catch (EOFException e) {
-            log.log(Levels.WARNING, "Unexpected EOF", e);
-        }
-        if (event == null) {
-            return null;
-        }
-        while ((tid != -1 && event.getTid() != tid && (event instanceof StepEvent || event instanceof InterruptEvent)) || (event instanceof DeviceDefinitionEvent)) {
-            if (event instanceof DeviceDefinitionEvent) {
-                analysis.process(event, event);
-            }
-            event = in.read();
-            if (event == null) {
-                return null;
-            }
-        }
-        if (tid == -1) {
-            tid = event.getTid();
-        }
-        if (event instanceof StepEvent) {
-            StepEvent step = (StepEvent) event;
-            if (step.getMachinecode() != null && (step.isCall() || (system && step.isSyscall()))) {
-                BlockNode block = new BlockNode(step);
-                analysis.process(event, block);
-                if (progress != null) {
-                    progress.progressUpdate(in.tell());
-                }
-                List<Node> result = new ArrayList<>();
-                boolean hasSteps = false;
-                int cnt = 0;
-                boolean ignore = true;
-                while (true) {
-                    Node child = parseRecord(in, analysis, progress, tid, ignore);
-                    if (child == null) {
-                        if (!hasSteps) {
-                            result.add(new IncompleteTraceStep(tid));
-                        }
-                        break;
-                    }
-                    if (isStep(child)) {
-                        result.add(child);
-                    }
-                    if (progress != null && cnt > PROGRESS_UPDATE) {
-                        cnt = 0;
-                        progress.progressUpdate(in.tell());
-                    } else {
-                        cnt++;
-                    }
-                    if (child instanceof StepEvent) {
-                        hasSteps = true;
-                        ignore = false;
-                        StepEvent s = (StepEvent) child;
-                        if (s.getMachinecode() == null || (s.isReturn() || (system && s.isReturnFromSyscall()))) {
-                            if (progress != null) {
-                                progress.progressUpdate(in.tell());
-                            }
-                            break;
-                        }
-                    } else if (child instanceof BlockNode) {
-                        hasSteps = true;
-                        ignore = false;
-                        BlockNode n = (BlockNode) child;
-                        int size = n.getNodes().size();
-                        if (!stackedTraps && system && n.getHead() != null && !n.isInterrupt() && !n.getHead().isSyscall() && size > 0) {
-                            Node last = n.getNodes().get(size - 1);
-                            while (last instanceof BlockNode) {
-                                BlockNode b = (BlockNode) last;
-                                if (b.isInterrupt() || b.getHead().isSyscall()) {
-                                    break;
-                                } else {
-                                    size = b.getNodes().size();
-                                    if (size > 0) {
-                                        last = b.getNodes().get(size - 1);
-                                    } else {
-                                        break;
-                                    }
-                                }
-                            }
-                            if (last instanceof StepEvent) {
-                                StepEvent s = (StepEvent) last;
-                                if (s.isReturnFromSyscall()) {
-                                    log.log(Levels.DEBUG, () -> String.format("break: %d [%x] in %d [%x]\n", s.getStep(), s.getPC(), step.getStep(), step.getPC()));
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                block.setChildren(result);
-                return block;
-            } else {
-                Event node = event;
-                analysis.process(event, node);
-                return node;
-            }
-        } else if (!ignoreTrap && system && event instanceof InterruptEvent) {
-            InterruptEvent interrupt = (InterruptEvent) event;
-            BlockNode block = new BlockNode(interrupt);
-            analysis.process(event, block);
-            if (progress != null) {
-                progress.progressUpdate(in.tell());
-            }
-            List<Node> result = new ArrayList<>();
-            boolean hasSteps = false;
-            int cnt = 0;
-            while (true) {
-                Node child = parseRecord(in, analysis, progress, tid, false);
-                if (child == null) {
-                    if (!hasSteps) {
-                        result.add(new IncompleteTraceStep(tid));
-                    }
-                    break;
-                }
-                if (isStep(child)) {
-                    result.add(child);
-                }
-                if (progress != null && cnt > 10_000) {
-                    cnt = 0;
-                    progress.progressUpdate(in.tell());
-                } else {
-                    cnt++;
-                }
-                if (child instanceof StepEvent) {
-                    hasSteps = true;
-                    StepEvent s = (StepEvent) child;
-                    if (s.getMachinecode() == null || s.isReturnFromSyscall()) {
-                        if (progress != null) {
-                            progress.progressUpdate(in.tell());
-                        }
-                        break;
-                    }
-                } else if (child instanceof BlockNode) {
-                    hasSteps = true;
-                    BlockNode n = (BlockNode) child;
-                    int size = n.getNodes().size();
-                    if (!stackedTraps && system && n.getHead() != null && !n.isInterrupt() && !n.getHead().isSyscall() && size > 0) {
-                        Node last = n.getNodes().get(size - 1);
-                        while (last instanceof BlockNode) {
-                            BlockNode b = (BlockNode) last;
-                            if (b.isInterrupt() || b.getHead().isSyscall()) {
-                                break;
-                            } else {
-                                size = b.getNodes().size();
-                                if (size > 0) {
-                                    last = b.getNodes().get(size - 1);
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                        if (last instanceof StepEvent) {
-                            StepEvent s = (StepEvent) last;
-                            if (s.isReturnFromSyscall()) {
-                                log.log(Levels.DEBUG, () -> String.format("break trap: %d [%x]\n", s.getStep(), s.getPC()));
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            block.setChildren(result);
-            return block;
-        } else {
-            Event node = event;
-            analysis.process(event, node);
-            return node;
-        }
     }
 
     @Override
